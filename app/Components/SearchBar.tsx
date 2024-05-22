4d01675ab1aa7ba8aff4cb7b4cf7705f66183b41
bgr7 <bgrbaygul@gmail.com>
2024-05-22 09:46:34 +0300

    lastsearchbar

--- app/Components/SearchBar.tsx
+++ app/Components/SearchBar.tsx
@@ -22,7 +22,7 @@ export default function SearchBar() {
           type="text"
           value={query}
           onChange={(e) => setQuery(e.target.value)}
-          placeholder="Enter Barcode Number..."
+          placeholder="Search Barcode No."
           className="border border-gray-300 rounded-lg p-2 mr-3 w-full"
         />
         <button
